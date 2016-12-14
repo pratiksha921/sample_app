7acb0187cfcdaaa5a1b2dc83990ebeb78f179494
pratiksha <pratiksha.wighe@tcs.com>
2016-12-14 16:08:31 +0530

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,10 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-  get  'static_pages/home'
-  get  'static_pages/help'
-  root 'application#hello'
-=======
-  get 'users/new'
 
+ 
   root 'static_pages#home'
   get  '/help',    to: 'static_pages#help'
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
- get  '/signup',  to: 'users#new'
->>>>>>> modeling-users
+  get  '/signup',  to: 'users#new'
+  resources :users
 end
